a173428c758b4e6c0628133f789c663cc58712dd
LancerFDa <rodrgolorenzo@gmail.com>
2026-01-28 16:54:38 +0100

    refactor(yatzy_fullHouse): Cambiar nombres de variables y refactorizar fullHouse()
    Co-authored-by: FernanPC18 <FernanPC18@users.noreply.github.com>

--- src/yatzy.py
+++ src/yatzy.py
@@ -178,33 +178,19 @@ class Yatzy:
     def large_straight(*dice):
         return Yatzy.calculate_straights(*dice)
 
+
+    '''
+    Rename a variable with a clearer name
+    The rutine was too long
+    '''
     @staticmethod
-    def fullHouse(d1, d2, d3, d4, d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0] * 6
-        tallies[d1 - 1] += 1
-        tallies[d2 - 1] += 1
-        tallies[d3 - 1] += 1
-        tallies[d4 - 1] += 1
-        tallies[d5 - 1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2):
-                _2 = True
-                _2_at = i + 1
-
-        for i in range(6):
-            if (tallies[i] == 3):
-                _3 = True
-                _3_at = i + 1
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
+    def fullHouse(*dice):
+        THREE_OF_A_KIND = Hands.THREE_OF_A_KIND.value
+        PAIR = Hands.PAIR.value
+        unique_dice = set(dice)
+        if len(unique_dice) == 2:
+            first_die = unique_dice.pop()
+            second_die = unique_dice.pop()
+            if (dice.count(first_die) == THREE_OF_A_KIND and dice.count(second_die) == PAIR):
+                return sum(dice)
+        return 0

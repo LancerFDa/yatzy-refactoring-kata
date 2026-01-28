5e67e1349e2ff7c5b234eaf8cda535a69023a139
LancerFDa <rodrgolorenzo@gmail.com>
2026-01-28 10:19:03 +0100

    refactor(yatzy_trhee_of_o_kind): Cambiar nombres de variables, refactorizar three_of_a_kind() y crear clase
    Co-authored-by: FernanPC18 <FernanPC18@users.noreply.github.com>

--- src/pips.py
+++ src/pips.py
@@ -24,6 +24,7 @@ class Pips(Enum):
 
 class Hands(Enum):
     PAIR = 2
+    THREE_OF_A_KIND = 3
     FOUR_OF_A_KIND = 4
 
 if __name__ == "__main__":
--- src/yatzy.py
+++ src/yatzy.py
@@ -134,16 +134,10 @@ class Yatzy:
         return 0
 
     @staticmethod
-    def three_of_a_kind(d1, d2, d3, d4, d5):
-        t = [0] * 6
-        t[d1 - 1] += 1
-        t[d2 - 1] += 1
-        t[d3 - 1] += 1
-        t[d4 - 1] += 1
-        t[d5 - 1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i + 1) * 3
+    def three_of_a_kind(*dice):
+        for die in Pips.reversedValues():
+            if dice.count(die) >= Hands.THREE_OF_A_KIND.value:
+                return die * 3
         return 0
 
 
